119203e83c81270266c219fc00969b97a8d086fd
Karan Singh <125845918+karansingh007-dev@users.noreply.github.com>
2025-07-28 15:51:13 +0530

    Update SongsItem.jsx

--- spotify-frontend/src/components/SongsItem.jsx
+++ spotify-frontend/src/components/SongsItem.jsx
@@ -7,9 +7,13 @@ function SongsItem({ image, name, desc, id }) {
     <div
       key={id}
       onClick={() => playWithId(id)}
-      className="min-w-[180px]  p-2 px-3 h-[150px] w-[210px] rounded cursor-pointer hover:bg-[#ffffff26]"
+      className="min-w-[180px]   h-[180px] w-[210px] rounded cursor-pointer hover:bg-[#ffffff26]"
     >
-      <img className="rounded w-full h-full object-contain" src={image} alt="album img" />
+      <img
+        className="rounded w-full h-full object-contain"
+        src={image}
+        alt="album img"
+      />
       <p className="font-bold mt-2 mb-1">{name}</p>
       <p className="text-slate-200 text-sm">{desc}</p>
     </div>
