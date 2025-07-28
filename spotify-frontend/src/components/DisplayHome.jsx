82c9540684eb1b8d80d435681b5c9af6a64e43df
Karan Singh <125845918+karansingh007-dev@users.noreply.github.com>
2025-07-28 16:10:12 +0530

    Update DisplayHome.jsx

--- spotify-frontend/src/components/DisplayHome.jsx
+++ spotify-frontend/src/components/DisplayHome.jsx
@@ -11,7 +11,7 @@ function DisplayHome() {
       <Navbar />
       <div className="mb-4 h-60 ">
         <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
-        <div className="flex h-50 overflow-auto scrollbar-hide">
+        <div className="flex h-60 overflow-auto scrollbar-hide">
           {albumsData.map((item, index) => (
             <AlbumItem
               key={index}
