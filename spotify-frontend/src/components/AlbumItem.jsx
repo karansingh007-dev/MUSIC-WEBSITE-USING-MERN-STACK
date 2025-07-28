80b38c7c40840da885e292716092bb0e2e56f932
Karan Singh <125845918+karansingh007-dev@users.noreply.github.com>
2025-07-28 15:11:14 +0530

    Update AlbumItem.jsx

--- spotify-frontend/src/components/AlbumItem.jsx
+++ spotify-frontend/src/components/AlbumItem.jsx
@@ -1,14 +1,22 @@
 import { useNavigate } from "react-router-dom";
 
 function AlbumItem({ image, name, desc, id }) {
-    const navigate = useNavigate();
-    return (
-        <div key={id} onClick={() => navigate(`/album/${id}`)} className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
-            <img className="rounded" src={image} alt="album img" />
-            <p className="font-bold mt-2 mb-1">{name}</p>
-            <p className="text-slate-200 text-sm">{desc}</p>
-        </div>
-    )
+  const navigate = useNavigate();
+  return (
+    <div
+      key={id}
+      onClick={() => navigate(`/album/${id}`)}
+      className="min-w-[180px] p-2 px-3 h-[150px] w-[200px] rounded cursor-pointer  hover:bg-[#ffffff26]"
+    >
+      <img
+        className="rounded w-full h-full  object-contain "
+        src={image}
+        alt="album img"
+      />
+      <p className="font-bold mt-2 mb-1">{name}</p>
+      <p className="text-slate-200 text-sm">{desc}</p>
+    </div>
+  );
 }
 
-export default AlbumItem
+export default AlbumItem;
