7b8966218ee189a99a07597a8c7f91600e2a10cf
Karan Singh <125845918+karansingh007-dev@users.noreply.github.com>
2025-07-28 15:10:06 +0530

    Update DisplayHome.jsx

--- spotify-frontend/src/components/DisplayHome.jsx
+++ spotify-frontend/src/components/DisplayHome.jsx
@@ -1,28 +1,44 @@
-import { useContext } from "react"
-import AlbumItem from "./AlbumItem"
-import Navbar from "./Navbar"
-import SongsItem from "./SongsItem"
-import { PlayerContext } from './../context/PlayerContext';
+import { useContext } from "react";
+import AlbumItem from "./AlbumItem";
+import Navbar from "./Navbar";
+import SongsItem from "./SongsItem";
+import { PlayerContext } from "./../context/PlayerContext";
 
 function DisplayHome() {
-    const { songsData, albumsData } = useContext(PlayerContext)
-    return (
-        <>
-            <Navbar />
-            <div className="mb-4">
-                <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
-                <div className="flex overflow-auto">
-                    {albumsData.map((item, index) => (<AlbumItem key={index} image={item.image} name={item.name} desc={item.desc} id={item._id} />))}
-                </div>
-            </div>
-            <div className="mb-4">
-                <h1 className="my-5 font-bold text-2xl">Today&apos;s biggest hits</h1>
-                <div className="flex overflow-auto">
-                    {songsData.map((item, index) => (<SongsItem key={index} image={item.image} name={item.name} desc={item.desc} id={item._id} />))}
-                </div>
-            </div>
-        </>
-    )
+  const { songsData, albumsData } = useContext(PlayerContext);
+  return (
+    <>
+      <Navbar />
+      <div className="mb-4 h-60 ">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex h-50 overflow-auto scrollbar-hide">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+              id={item._id}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4 h-60 ">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex h-50 overflow-auto scrollbar-hide">
+          {songsData.map((item, index) => (
+            <SongsItem
+              key={index}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+              id={item._id}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default DisplayHome
+export default DisplayHome;
